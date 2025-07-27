41e1adb2de492ff0885dce7e21fa9577db31e995
Alexandra Pastor <pastoralex@free.fr>
2025-07-27 17:52:41 +0200

    fix import swiper

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,8 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import '../../node_modules/swiper/swiper.css';
+import '../../node_modules/swiper/modules/pagination.css';
+import '../../node_modules/swiper/modules/navigation.css';
 
 import ZoomImage from '../components/ZoomImage';
 
